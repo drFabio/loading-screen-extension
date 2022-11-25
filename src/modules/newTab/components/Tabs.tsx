9ee5faf59120c36bbf662f0ee87b6458c70a46b0
Fabio Oliveira Costa <f.costa@visi-one.com>
2022-11-25 17:27:34 +0100

    feat: adds css modules

--- src/modules/newTab/components/Tabs.tsx
+++ src/modules/newTab/components/Tabs.tsx
@@ -3,13 +3,18 @@ import { sources } from "../../../assets/de-en/sources";
 import { SourceTypes } from "../../../types";
 import { Equivalence } from "./Equivalence";
 import { useDataSources } from "./useDataSources";
+import * as classes from "./Tabs.module.css";
 
 export function Tabs() {
   const { type, choice } = useDataSources(sources);
+  let container: JSX.Element;
 
   if (type === SourceTypes.EQUIVALENCE) {
-    const [term, definition] = choice;
-    return <Equivalence term={term} definition={definition} />;
+    const [term, definition] = choice as [string, string];
+    container = <Equivalence term={term} definition={definition} />;
+  } else {
+    container = <></>;
   }
-  return <h1>Hello world!</h1>;
+
+  return <main className={classes.container}>{container}</main>;
 }

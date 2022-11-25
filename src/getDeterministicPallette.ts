ae5067302bc83e6ca5fe164e046e8372774564a4
Fabio Oliveira Costa <f.costa@visi-one.com>
2022-11-25 18:06:25 +0100

    feat: associate input with deterministic color

---
+++ src/getDeterministicPallette.ts
@@ -0,0 +1,57 @@
+const ALLOWED_COLORS = [
+  "#001f3f",
+  "#0074d9",
+  "#7fdbff",
+  "#39cccc",
+  "#3d9970",
+  "#2ecc40",
+  "#01ff70",
+  "#ffdc00",
+  "#ff851b",
+  "#ff4136",
+  "#85144b",
+  "#f012be",
+  "#b10dc9",
+];
+
+const TOTAL_COLORS = ALLOWED_COLORS.length;
+
+/**
+ * Given any serializable input returns a legible fg and bg. Same input = same color
+ * @param input
+ * @returns a deterministic color for that input
+ */
+export function getDeterministicPallette(input: any) {
+  const serializedString = JSON.stringify(input);
+  const colorIndex = serializedString
+    .split("")
+    .reduce((acc, char) => (acc + char.charCodeAt(0)) % TOTAL_COLORS, 0);
+  const deterministicColor = ALLOWED_COLORS[colorIndex];
+  const color = getForegroundColor(deterministicColor);
+  return { color, backgroundColor: deterministicColor };
+}
+
+/**
+ * (c) 2019 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * Derived from work by  https://gomakethings.com/dynamically-changing-the-text-color-based-on-background-color-contrast-with-vanilla-js/
+ *@see {https://gomakethings.com/dynamically-changing-the-text-color-based-on-background-color-contrast-with-vanilla-js/}
+ */
+const getForegroundColor = (hexInput: string) => {
+  let hex = hexInput.replace("#", "");
+  if (hex.length == 3) {
+    hex = hex.split("").reduce((acc, char) => `${acc}${char}${char}`, "");
+  }
+  const [r, g, b] = /^([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i
+    .exec(hex)
+    .slice(1)
+    .map((color) => parseInt(color, 16));
+  /**
+   * @see {@link https://en.wikipedia.org/wiki/YIQ}
+   */
+  const yiq = (r * 299 + g * 587 + b * 114) / 1000;
+  /**
+   * @todo it would be nice to get a complementary foreground that was high contrast
+   * alas, I am no designer so this will do for now
+   */
+  return yiq >= 128 ? "#000000" : "#ffffff";
+};
--- src/modules/newTab/components/Tabs.tsx
+++ src/modules/newTab/components/Tabs.tsx
@@ -4,10 +4,17 @@ import { SourceTypes } from "../../../types";
 import { Equivalence } from "./Equivalence";
 import { useDataSources } from "./useDataSources";
 import * as classes from "./Tabs.module.css";
+import { getDeterministicPallette } from "../../../getDeterministicPallette";
 
 export function Tabs() {
   const { type, choice } = useDataSources(sources);
   let container: JSX.Element;
+
+  /**
+   * We want to have the same color for the same input
+   * So people can associate them better in case they show up more than once
+   */
+  const { color, backgroundColor } = getDeterministicPallette(choice);
 
   if (type === SourceTypes.EQUIVALENCE) {
     const [term, definition] = choice as [string, string];
@@ -16,5 +23,9 @@ export function Tabs() {
     container = <></>;
   }
 
-  return <main className={classes.container}>{container}</main>;
+  return (
+    <main className={classes.container} style={{ color, backgroundColor }}>
+      {container}
+    </main>
+  );
 }

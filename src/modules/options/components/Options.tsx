3f1c7877160bde2b1b2d663b0ac564f198b8edea
Fabio Oliveira Costa <f.costa@visi-one.com>
2022-11-27 14:39:51 +0100

    feat: starts options

--- src/modules/options/components/Options.tsx
+++ src/modules/options/components/Options.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import * as classes from "./Options.module.scss";
 
 export function Options() {
-  return <h1>Hello world!</h1>;
+  return (
+    <main className={classes.container}>
+      <section>
+        <h2>Available Sources</h2>
+      </section>
+    </main>
+  );
 }

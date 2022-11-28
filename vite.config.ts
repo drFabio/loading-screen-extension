f6b70f9cf551414aff99bc01c4785d39e18a9005
Fabio Oliveira Costa <f.costa@visi-one.com>
2022-11-28 11:00:26 +0100

    chore: removes base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://drfabio.github.io/loading-screen-extension-gh-pages",
+  // base: "https://drfabio.github.io/loading-screen-extension-gh-pages",
   plugins: [react()],
   build: {
     rollupOptions: {
